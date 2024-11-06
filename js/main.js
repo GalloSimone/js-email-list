fcbb9374a74db5cb15f0aeb7e084218be86538ef
GalloSimone <gallosimonecz@gmail.com>
2024-11-06 16:39:15 +0100

    aggiunta funzione di generazione di 10 email casuali

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
-const tfy=("tyyy")
-console.log(tfy);
+//recupero id list
+    const listEmail= document.getElementById("email-list") 
+//creo il ciclo di generazione di 10 email
+    for (let i = 0; i < 10; i++) {
+        fetch('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then(result => result.json())
+    .then( (data) => {
+       const Email=data.response;
+    //sostituisco nell'html js
+    listEmail.innerHTML +=`<li>${Email}</li>`;
+})
+
+}
